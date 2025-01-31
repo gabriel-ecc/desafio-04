96ec35282792146942d0bcd378913cc339d818f1
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-01-30 21:16:12 -0300

    version entregable

--- src/components/CardPizza.jsx
+++ src/components/CardPizza.jsx
@@ -1,23 +1,39 @@
-
-
-
-const CardPizza = ({name, price, ingredients, url}) => {
-    let textoIngredientes = "";
-    for (const ingrediente of ingredients) {
-        textoIngredientes += " "+ingrediente;
+const CardPizza = ({ name, price, ingredients, url }) => {
+  let textoIngredientes = "";
+  for (let i = 0; i < ingredients.length; i++) {
+    textoIngredientes += ingredients[i];
+    if (i < ingredients.length - 1) {
+      textoIngredientes += ", ";
     }
-    textoIngredientes+= "."
-    
+  }
+  textoIngredientes += ".";
 
-    return (
-        <div className="card w-25">
-            <img className="card-img-top" src={url} alt={name}/>
-            <div className="card-body">
-                <h5 className="card-title">{name}</h5>
-                <p className="card-text text-dark">{textoIngredientes}</p>
-                <a href="#" className="btn btn-primary">Go somewhere</a>
-            </div>
-        </div>
-    );
-}
+  return (
+    <div className='card col-12 col-sm-6 col-md-4 col-lg-3 mb-4'>
+      <img src={url} className='card-img-top' alt={name} />
+      <div className='card-body'>
+        <h5 className='card-title fw-semibold fs-5'>Pizza {name}</h5>
+      </div>
+      <ul className='list-group list-group-flush'>
+        <li className='list-group-item'>
+          <p className='card-text text-center'>Ingredientes:</p>
+          <p className='card-text text-center fs-6'>🍕 {textoIngredientes}</p>
+        </li>
+        <li className='list-group-item'>
+          <p className='text-center fw-semibold fs-3'>
+            Precio: ${price.toLocaleString("es-cl")}
+          </p>
+        </li>
+      </ul>
+      <div className='card-body d-flex justify-content-between'>
+        <a href='#' className='btn btn-light'>
+          Ver Más 👀
+        </a>
+        <a href='#' className='btn btn-dark'>
+          Añadir 🛒
+        </a>
+      </div>
+    </div>
+  );
+};
 export default CardPizza;
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <>
-        <h2 className="text-light bg-dark text-center fs-6">© 2021 - Pizzería Mamma Mia! - Todos los derechos reservados</h2>
+        <h2 className="text-light bg-dark text-center fs-6 p-4">© 2021 - Pizzería Mamma Mia! - Todos los derechos reservados</h2>
         </>
     );
 }

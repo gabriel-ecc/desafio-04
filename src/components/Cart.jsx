d08a43828cb0c88cb0e9cd0d2883b20e05063fb8
gabriel <gabriel.castillo.cardenas@gmail.com>
2025-02-16 23:09:02 -0300

    hito 3 completo version entregable

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -2,14 +2,6 @@ import { useState } from "react";
 import { pizzaCart } from "../assets/js/pizzas";
 
 const Cart = () => {
-  const updateTotal = (lista) => {
-    let monto = 0;
-    for (const pizza of lista) {
-      monto += pizza.count * pizza.price;
-    }
-    return monto;
-  };
-
   const [miCarrito, setMiCarrito] = useState(pizzaCart);
   const [totalCarrito, setTotalCarrito] = useState(updateTotal(miCarrito));
 
@@ -17,18 +9,36 @@ const Cart = () => {
     setPizza((prev) => ({ ...prev, [e.target.id]: e.target.value }));
   };
 
-  const agregarCantidad = (id) => {
-    
+  const agregarCantidad = (p) => {
+    p.count += 1;
+    const nuevaLista = miCarrito.filter((pizza) => pizza.id != p.id);
+    setMiCarrito([...nuevaLista, p]);
+    setTotalCarrito(updateTotal(miCarrito))
   };
 
-  const diminuirCantidad = (id) => {
-    
+  const diminuirCantidad = (p) => {
+    if (p.count > 1) {
+      p.count -= 1;
+      const nuevaLista = miCarrito.filter((pizza) => pizza.id != p.id);
+      setMiCarrito([...nuevaLista, p]);
+      setTotalCarrito(updateTotal(miCarrito))
+    }
   };
+
+  const productosOrdenados = [...miCarrito].sort((a, b) => {
+    if (a.id < b.id) {
+      return -1;
+    }
+    if (a.id > b.id) {
+      return 1;
+    }
+    return 0;
+  });
 
   return (
     <>
-      <div id='carrito' className="p-2">
-        {miCarrito.map((p) => (
+      <div id='carrito' className='p-2'>
+        {productosOrdenados.map(p => (
           <div className='card mb-3' key={p.id}>
             <div className='row g-0'>
               <div className='col-md-4'>
@@ -36,19 +46,21 @@ const Cart = () => {
               </div>
               <div className='col-md-8'>
                 <div className='card-body d-flex justify-content-around align-self-center'>
-                  <h5 className="card-title">{p.name}</h5>
-                  <p className="card-text">${p.price.toLocaleString("es-cl")}</p>
+                  <h5 className='card-title'>{p.name}</h5>
+                  <p className='card-text'>
+                    ${p.price.toLocaleString("es-cl")}
+                  </p>
                   <button
                     type='button'
                     className='btn btn-danger'
-                    onClick={diminuirCantidad(p.id)}>
+                    onClick={() => diminuirCantidad(p)}>
                     -
                   </button>
-                  <p className="card-text">{p.count}</p>
+                  <p className='card-text'>{p.count}</p>
                   <button
                     type='button'
                     className='btn btn-success'
-                    onClick={agregarCantidad(p.id)}>
+                    onClick={() => agregarCantidad(p)}>
                     +
                   </button>
                 </div>
@@ -57,9 +69,19 @@ const Cart = () => {
           </div>
         ))}
       </div>
-      <p className="fs-1 text-info">Total: ${totalCarrito.toLocaleString("es-cl")}</p>
+      <p className='fs-1 text-info'>
+        Total: ${totalCarrito.toLocaleString("es-cl")}
+      </p>
     </>
   );
 };
 
 export default Cart;
+
+const updateTotal = (lista) => {
+  let monto = 0;
+  for (const pizza of lista) {
+    monto += pizza.count * pizza.price;
+  }
+  return monto;
+};
